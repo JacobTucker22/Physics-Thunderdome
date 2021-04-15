3edec1752941693efe081f7f6af6f9e1b66a7749
JacobTucker22 <72776180+JacobTucker22@users.noreply.github.com>
2021-04-15 13:13:06 -0700

    Working on Main Menu UI. Slider changes text for number of enemies

---
+++ Assets/Scripts/SliderUpdate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderUpdate : MonoBehaviour
+{
+     public Text numOfEnemies;
+     public Slider numEnemiesSlider;
+
+     public void updateNumberOfEnemies()
+     {
+          numOfEnemies.text = numEnemiesSlider.value.ToString();
+     }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
