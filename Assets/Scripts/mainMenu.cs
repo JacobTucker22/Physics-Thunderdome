531ef9aeed64af483ca6b275697e8d0d038030ad
JacobTucker22 <72776180+JacobTucker22@users.noreply.github.com>
2021-04-15 14:26:34 -0700

    Rough UI complete
    Slider changes number of enemies variable(to be used later). Level buttons change chosen level. Start button loads chosen level.

---
+++ Assets/Scripts/mainMenu.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour
+{
+
+     public string chosenScene = null;
+
+     public int numOfEnemies;
+     public Text numOfEnemiesText;
+     public Slider numEnemiesSlider;
+
+     public Text selectedLevelText;
+     public Button tunnelButton, domeButton, startButton;
+
+     public void updateNumberOfEnemies()
+     {
+          numOfEnemiesText.text = numEnemiesSlider.value.ToString();
+          numOfEnemies = (int)numEnemiesSlider.value;
+     }
+
+     public void selectTunnel()
+     {
+          chosenScene = tunnelButton.GetComponentInParent<Text>().text;
+          selectedLevelText.text = chosenScene;
+     }
+
+     public void selectDome()
+     {
+          chosenScene = domeButton.GetComponentInParent<Text>().text;
+          selectedLevelText.text = chosenScene;
+     }
+
+     public void StartGame()
+     {
+          if(chosenScene == null)
+          {
+               Debug.Log("No Level Selected");
+          }
+          else
+          {
+               SceneManager.LoadScene(chosenScene);
+          }
+
+     }
+}
